f24e8e832e3af914bbe39ee25c36a935fcef5d64
Milad Sharif <milad-sharif@hotmail.no>
2014-11-10 19:33:01 +0100

    hip hip hurra hehe ferdig med forsiden :)

---
+++ indexstyle.js
@@ -0,0 +1,12 @@
+//vidu skal kalle på showMenu for hver skroll
+window.addEventListener("scroll", showMenu);
+
+function showMenu(){
+	var limitElement = document.getElementById("nyheter-main");
+	var scrollValue = window.scrollY;
+
+	if(scrollValue >= limitElement.offsetTop - 100) /*pass på denne!*/
+		document.getElementById("second-menu").style.display = "block";
+	else
+		document.getElementById("second-menu").style.display = "none";	
+}
